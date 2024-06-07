2acf9352a6e0e16c5a83f3797e1a70e6a8c60579
Muhammad sumair <samomuhammadsumair@gmail.com>
2024-06-07 00:12:35 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const currency = {
+    USD: 1, // base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: "Enter From Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'to',
+        message: "Enter To Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'amount',
+        message: "Enter Your Amount",
+        type: "number",
+    }
+]);
+ 
+
+
+let fromamount = currency[user_answer.from];
+let toamount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromamount; // usd base currency 
+let convertedAmount = baseAmount * toamount;
+let roundNumber = convertedAmount.toFixed(2);
+console.log(roundNumber);
